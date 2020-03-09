ccadccaff96cb643d776f6a5e775c6c7e6c806ab
guilhermegrg <guilhermegrg@gmail.com>
2020-03-09 17:13:14 +0000

    Tried the calendar but it was waaaay too complex. Need to code my stuff again!

--- admin/admin-footer.php
+++ admin/admin-footer.php
@@ -1,8 +1,8 @@
 
-   
-       </div>
+
+                    </div>
                 </main>
-                <footer class="py-4 bg-light mt-auto">
+                   <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; Your Website 2019</div>
@@ -16,6 +16,25 @@
                 </footer>
             </div>
         </div>
+        <script src="../vendors/jquery-3.4.1.min.js"></script>
+        <script src="../vendors/bootstrap-4.4.1-dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/datatables-demo.js"></script>
+    </body>
+</html>
+   
+      
+      
+<!--
+      
+       </div>
+                </main>
+                
+            </div>
+        </div>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
@@ -27,6 +46,7 @@
         <script src="assets/demo/datatables-demo.js"></script>
     </body>
 </html>
+-->
    
 <!--
    <script src="../vendors/jquery-3.4.1.min.js"></script>
